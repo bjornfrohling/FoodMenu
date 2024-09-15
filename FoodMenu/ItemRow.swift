2e73fcb31796d6ebd7ca5fec03187030692b567c
Björn Fröhling <bjoern.froehling@gmail.com>
2024-09-15 18:25:16 +0300

    Added ItemRow View

---
+++ FoodMenu/ItemRow.swift
@@ -0,0 +1,18 @@
+//
+//  ItemRow.swift
+//  FoodMenu
+//
+//  Created by bjorn at work on 2024-09-15.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemRow()
+}
