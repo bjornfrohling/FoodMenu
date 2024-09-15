778c4bc3f8eb492e3e1ba6315edd7fd8233e6b55
Björn Fröhling <bjoern.froehling@gmail.com>
2024-09-15 12:46:25 +0300

    Initial Commit

---
+++ FoodMenu/FoodMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMenuApp.swift
+//  FoodMenu
+//
+//  Created by bjorn at work on 2024-09-15.
+//
+
+import SwiftUI
+
+@main
+struct FoodMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
