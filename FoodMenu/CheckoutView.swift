cfa79d39eab7e48acb7b262929cec7442bdc3ffb
Björn Fröhling <bjoern.froehling@gmail.com>
2024-09-16 21:40:55 +0300

    Added CheckoutView

---
+++ FoodMenu/CheckoutView.swift
@@ -0,0 +1,73 @@
+//
+//  CheckoutView.swift
+//  FoodMenu
+//
+//  Created by bjorn at work on 2024-09-16.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @EnvironmentObject var order: Order
+    
+    let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    let tipAmounts = [10, 15, 20, 25, 0]
+    
+    @State private var paymentType = "Cash"
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var tipAmount = 15
+    @State private var showingPaymentAlert = false
+    
+    var totalPrice: String {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+        return (total + tipValue).formatted(.currency(code: "USD"))
+    }
+    
+    var body: some View {
+        Form {
+            Section {
+                Picker("How do you want to pay?", selection: $paymentType) {
+                    ForEach(paymentTypes, id: \.self) {
+                        Text($0)
+                    }
+                }
+                Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
+                if addLoyaltyDetails {
+                    TextField("Enter your iDine ID", text: $loyaltyNumber)
+                } 
+            }
+            Section("Add a tip?") {
+                Picker("Percentage:", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self) {
+                        Text("\($0)%")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            Section("Total: \(totalPrice)") {                Button("Confirm order") {
+                    // place the order
+                showingPaymentAlert = true
+                }
+            }
+        }
+        .alert("Order confirmed", isPresented: $showingPaymentAlert) {
+            Button("Confirm order") {
+                showingPaymentAlert.toggle()
+            }        } message: {
+            Text("Your total was \(totalPrice) – thank you!")
+        }
+        .navigationTitle("Payment")
+        .navigationBarTitleDisplayMode(.inline)    }
+}
+
+#Preview {
+    CheckoutView()
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView().environmentObject(Order())
+    }
+}
